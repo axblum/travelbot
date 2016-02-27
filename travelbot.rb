2449bab803e92c6fe60afcbe98e6b63014272c7e
Andrew Blum <andrew.blum@gmail.com>
2016-02-27 11:44:33 -0800

    i am damn hot mess

--- travelbot.rb
+++ travelbot.rb
@@ -1,5 +1,6 @@
 require 'slack-ruby-bot'
 require 'pp'
+require 'uri'
 
 class TravelBot < SlackRubyBot::Bot
   def self.flight_search
@@ -10,24 +11,34 @@ class TravelBot < SlackRubyBot::Bot
     offers = response["offers"]
     offer_top_ten = []
     legs_ids = []
-    offers_ids = []
-    # top ten legIds for cheapest flights
-    for i in 0...10
-      offers_ids << offers[i]["legIds"]
+    # offers_ids = []
+    # top ten legIds  for i in 0...10
+    # offers.each do |offer|
+    #   offers_ids << offers[i]["legIds"]
       # offer_top_ten << offers[i]["totalFarePrice"]["formattedPrice"]
-    end
-    flight = []
+    flights = []
+
     legs.each do |leg|
-      if offers_ids.first == leg["legId"]
-        flight = leg["legId"]
+      offers.each do |offer|
+        if offer['legIds'].first == leg["legId"]
+           flights << {price: offer["totalFarePrice"]["formattedPrice"], link: offer["detailsUrl"]}
+
+        end
       end
     end
     # offers.each do |offer|
-    #   offer_prices << offer["totalFarePrice"]["formattedPrice"]
-    # end
-    return flight
-  end
 
+    # end
+    return "<a href=#{flights.first[:link]}Book Now>"
+  end
+  def self.shorten(long_url)
+    enc = CGI::escape(long_url)
+    bitlyendpoint = "https://api-ssl.bitly.com/v3/shorten?access_token=6836af0100785631ed7e8338c6adbe9729fed013&longUrl=#{enc}"
+    p bitlyendpoint
+    response = HTTParty.get(bitlyendpoint)
+    p response
+    #response["data"]["url"]
+  end
   # formatted total fare: response['offers'].first["totalFarePrice"]["formattedPrice"]
 
   # {formattedPrice:
@@ -40,8 +51,7 @@ class TravelBot < SlackRubyBot::Bot
 
   command 'ping' do |client, data, match|
     response = flight_search
-    p " -- " * 20
-    pp response
+    p shorten("http://google.com/")
     client.say(text: response, channel: data.channel)
   end
 
