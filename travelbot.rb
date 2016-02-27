fd074ea89cf21a0a5fe89de8907a68b89baa94fb
Andrew Blum <andrew.blum@gmail.com>
2016-02-26 16:39:00 -0800

    fix merge conflicts and add httparty

--- travelbot.rb
+++ travelbot.rb
@@ -1,6 +1,16 @@
 require 'slack-ruby-bot'
 
+
 class TravelBot < SlackRubyBot::Bot
+  # Instentiate api object
+  api = Expedia::Api.new
+
+  # Method to search for a hotel. see http://developer.ean.com/docs/hotel-list/
+  response = api.get_list({:propertyName => 'Hotel Moa', :destinationString => 'berlin'})
+
+  # execute this method to know if there is any exception
+  response.exception? # false if success
+  
   command 'ping' do |client, data, match|
     client.say(text: 'travel', channel: data.channel)
   end
