a9511ea92c87ceb4403908a95f298bd757c39d79
Andrew Blum <andrew.blum@gmail.com>
2016-02-26 17:14:14 -0800

    get base fare

--- travelbot.rb
+++ travelbot.rb
@@ -1,19 +1,26 @@
 require 'slack-ruby-bot'
-
+require 'pp'
 
 class TravelBot < SlackRubyBot::Bot
-  # Instentiate api object
-  api = Expedia::Api.new
+  def self.flight_search
+    url = "http://terminal2.expedia.com/x/mflights/search?departureAirport=LAX&arrivalAirport=ORD&departureDate=2016-04-22&childTravelerAge=2&apikey=#{ENV['EXPEDIA_KEY']}"
+    response = HTTParty.get(url)
+    response['offers'].first["baseFare"]
+  end
 
-  # Method to search for a hotel. see http://developer.ean.com/docs/hotel-list/
-  response = api.get_list({:propertyName => 'Hotel Moa', :destinationString => 'berlin'})
-
-  # execute this method to know if there is any exception
-  response.exception? # false if success
-  
   command 'ping' do |client, data, match|
-    client.say(text: 'travel', channel: data.channel)
+    response = flight_search
+    p " -- " * 20
+    pp response
+    client.say(text: response, channel: data.channel)
+  end
+
+  command 'test' do |client, data, match|
+    client.say(text: 'connected', channel: data.channel)
   end
 end
 
 TravelBot.run
+
+
+# http://terminal2.expedia.com/x/mflights/search?departureAirport=LAX&arrivalAirport=ORD&departureDate=2016-04-22&childTravelerAge=2&apikey=
